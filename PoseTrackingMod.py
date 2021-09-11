fdcc7b7b8981e4b691c6d7d22aa8aedf612b7b9e
Aashish1107 <aashishvennapusa1@gmail.com>
2021-09-11 22:25:27 +0530

    PoseTracking

--- PoseTrackingMod.py
+++ PoseTrackingMod.py
@@ -3,18 +3,24 @@ import mediapipe as mp
 import time
 
 class PoseDetector():
-    def __init__(self):
+    def __init__(self, mode=False ,complex=1 ,smoothlms=True ,enableSeg=False ,smooth=True ,detectionCon=0.5 ,trackCon=0.5):
         
+        self.mode=mode
+        self.complx=complex
+        self.smoothlms=smoothlms
+        self.enableSeg=enableSeg
+        self.smooth=smooth
+        self.detectionCon=detectionCon
+        self.trackCon=trackCon
+
 
         self.mpPose=mp.solutions.pose
-        self.pose=self.mpPose.Pose()
+        self.pose=self.mpPose.Pose(self.mode,self.complx,self.smoothlms,self.enableSeg,self.smooth,self.detectionCon,self.trackCon)
         self.mpdraw=mp.solutions.drawing_utils
 
     def findBody(self, img, draw=True):
         imgRGB=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results=self.pose.process(imgRGB)
-
-        
         if self.results.pose_landmarks:
             if draw == True:
                 self.mpdraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
@@ -25,19 +31,17 @@ class PoseDetector():
         
         lmList=[]
         if self.results.pose_landmarks:
-            myPose=self.results.pose_landmarks
-            
-            for id,lm in enumerate(myPose.pose_landmarks):
-                h,w,c=img.shape
-                cx, cy= int(lm.x*w), int(lm.y*h)
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c=img.shape
+                cx, cy=int(lm.x*w), int(lm.y*h)
                 lmList.append([id, cx, cy])
                 if draw:
-                    cv2.circle(img, (cx, cy), 15, (255,0,0), cv2.FILLED)
+                    cv2.circle(img, (cx, cy), 5,(255,0,0), cv2.FILLED)
         return lmList
 
 def main():
 
-    cap=cv2.VideoCapture(0)
+    cap=cv2.VideoCapture(r"C:\Users\Aashi\Downloads\t1.mp4")
     detector=PoseDetector()
     cTime=0
     pTime=0
@@ -46,7 +50,7 @@ def main():
         success, img=cap.read()
 
         img = detector.findBody(img)
-        lmList=[]
+        lmList=detector.findPosition(img)
         if(len(lmList)!=0):
             print(lmList[0])
 
