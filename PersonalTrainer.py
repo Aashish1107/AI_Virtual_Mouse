3ed0a225820b908d569c0882161d8b823c096748
Aashish1107 <aashishvennapusa1@gmail.com>
2021-09-19 23:33:23 +0530

    PersonalTrainer

--- PersonalTrainer.py
+++ PersonalTrainer.py
@@ -4,12 +4,16 @@ import PoseTrackingMod as ptm
 import math
 import numpy as np
 
+print("input 12,14 and 16 for right hand or 11,13,15 for left hand")
+#x,y,z are the 3 points to find the angle between xy and yz
+x,y,z=int(input()), int(input()), int(input())
 cap=cv2.VideoCapture(0)
 ptime=0
 
 detector=ptm.PoseDetector(detectionCon=0.75)
 dir=0
 count=0
+color=(0,255,0)
 
 while True:
     success, img=cap.read()
@@ -17,14 +21,16 @@ while True:
     img=detector.findBody(img,draw=False)
     lmList=detector.findPosition(img,draw=False)
     if len(lmList)!=0:
-        img,angle=detector.findAngle(img,12,14,16)
-        
+        img,angle=detector.findAngle(img,x,y,z)
+        if angle>180:
+            angle=360-angle
         per=np.interp(angle,[30,160],[100,0])
         bar=np.interp(angle,[30,160],[300,450])
+        
         #Check curls
         if per==100:
+            color=(255,0,255)
             if dir==0:
-                color=(255,0,255)
                 count+=0.5
                 dir=1
         if per==0:
